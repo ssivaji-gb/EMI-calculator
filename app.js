f87655f811a8d9b7f2b9ed301fb6b6668846a7e8
ssivaji-gb <sathirabathisivajir@gmail.com>
2026-01-10 09:56:50 +0530

    first commit

--- app.js
+++ app.js
@@ -1,13 +1,45 @@
-let form = document.querySelector(".emi-form");
+// simpleSi & compoundCi-page
+
+let simpleSi=document.querySelector(".emi-formsi")
+let compoundCi=document.querySelector(".compondintrestpage")
+
+
+let compound_btn=document.querySelector(".compound-interest")
+let simple_btn=document.querySelector(".simple-intrest")
+
+
+let form = document.querySelector(".emi-formsi");
 form.addEventListener("submit", (e) => {
   e.preventDefault();
-  let princepal = Number(document.querySelector(".princepal").value);
-  let interest = Number(document.querySelector(".interest").value);
-  let duration = Number(document.querySelector(".duration").value);
+  let princepal = Number(document.querySelector(".princepalsi").value);
+  let interest = Number(document.querySelector(".interestsi").value);
+  let duration = Number(document.querySelector(".durationsi").value);
 
+  // simple-Intrest
   let si = (princepal / 100) * interest * duration;
 
-  document.querySelector(".PrincipalAmountview").innerHTML = `${princepal}₹`
-  document.querySelector(".interestamountview").innerHTML = `${si}₹`
-  document.querySelector(".TotalPayableview").innerHTML = `${princepal + si}₹`;
+  document.querySelector(".PrincipalAmountviewsi").innerHTML = `${princepal}₹`
+  document.querySelector(".interestamountviewsi").innerHTML = `${si}₹`
+  document.querySelector(".TotalPayableviewsi").innerHTML = `${princepal + si}₹`;
 });
+
+
+
+
+
+// simplepage
+
+simple_btn.addEventListener("click",()=>{
+  simpleSi.style.display="block"
+  compoundCi.style.display="none"
+})
+
+// Compound
+
+compound_btn.addEventListener("click",()=>{
+  simpleSi.style.display="none"
+  compoundCi.style.display="block"
+})
+
+
+
